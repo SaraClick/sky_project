14af059c6ff1bd3ca079b7bfd530a22bbbb8a281
Mati <matimotof@gmail.com>
2023-04-08 21:30:42 +0100

    Added routes for contact, content_media, content_selection, index, tips

--- application/routes.py
+++ application/routes.py
@@ -9,3 +9,23 @@ DATA_PROVIDER = DataProviderService()
 @app.route("/home")
 def home():
     return render_template("index.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/content_media")
+def content_media():
+    return render_template("content_media.html")
+
+
+@app.route("/content_selection")
+def content_selection():
+    return render_template("content_selection.html")
+
+
+@app.route("/tips")
+def tips():
+    return render_template("tips.html")
