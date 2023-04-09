d7ed88ff612c109296d28a0068b6b1d3485042d2
Sara <saesbori@gmail.com>
2023-04-09 23:15:17 +0100

    comment removal from app.py

--- app.py
+++ app.py
@@ -6,6 +6,3 @@ app.config['SECRET_KEY'] = 'YOUR_SECRET_KEY'
 # Main trick
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
-
-# REMINDER!!!
-# Go onto data_provider_service.py and uncomment the line depending if you are MAC/WINDOWS user before running it
