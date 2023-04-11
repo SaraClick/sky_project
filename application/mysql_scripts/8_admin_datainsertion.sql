389527f3ddd938215509dede007064804fe499d1
Mati <matimotof@gmail.com>
2023-04-11 21:58:07 +0100

    Added data for admin table

---
+++ application/mysql_scripts/8_admin_datainsertion.sql
@@ -0,0 +1,8 @@
+use fortywinks;
+
+INSERT INTO admins(admin_username, admin_password, admin_status)
+VALUES('Mati', 'password1', 'active'),
+('Mirfat', 'password2', 'inactive'),
+('Sara', 'password3', 'inactive'),
+('Pammy', 'password4', 'active');
+
